2de6ecf10669b868fa83685995b8b9171659bb4f
M Xi <bongollmaria@gmail.com>
2025-03-26 15:22:10 +0100

    Conection with data base

---
+++ src/main/java/com/mvc/App.java
@@ -0,0 +1,16 @@
+package com.mvc;
+
+import com.mvc.config.DBManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Sabiduria-infinita" );
+        DBManager.initConnection();
+    }
+}
--- src/main/java/com/mvc/config/DBManager.java
+++ src/main/java/com/mvc/config/DBManager.java
@@ -1,5 +1,31 @@
 package com.mvc.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBManager {
-    
+    private static final String URL = "jdbc:postgresql://localhost:5432/sabiduria-infinita";
+    private static final String USER = "postgres";
+    private static final String PASS = "mWFyaWE=";
+
+    private static Connection connection;
+
+    public static Connection initConnection(){
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Se ha conectado con exito!");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }  
+        return connection;
+    }
+    public static void closeConnection(){
+        try {
+            connection.close();
+            System.out.println("Conexión cerrada correctamente");
+        } catch (Exception e) {
+            
+        }
+    }
 }
+
