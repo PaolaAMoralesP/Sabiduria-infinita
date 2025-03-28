7252081180e5c442e95f8f2b6d971c3af21cdfd4
M Xi <bongollmaria@gmail.com>
2025-03-28 09:27:54 +0100

    Fixed doble quotes and single quotes in String sql

--- src/main/java/com/mvc/model/BookDAO.java
+++ src/main/java/com/mvc/model/BookDAO.java
@@ -1,18 +1,19 @@
 package com.mvc.model;
 
-
 import java.sql.Connection;
 import java.sql.Statement;
 
 import com.mvc.config.DBManager;
 
 public class BookDAO {
-    
+
     public void createBook(Book book) {
         try {
             Connection connection = DBManager.initConnection();
             Statement stmn = connection.createStatement();
-            String sql = "INSERT INTO books (title, author, description, isbn, genre) VALUES ('" + book.getTitle() + "', '"+ book.getAuthor() + "', '" + book.getDescription() + "', '" + book.getIsbn() + "', '" + book.getGenre() ;
+            String sql = "INSERT INTO books (title, author, description, isbn, genre) VALUES ('" + book.getTitle()
+                    + "', '" + book.getAuthor() + "', '" + book.getDescription() + "', '" + book.getIsbn() + "', '"
+                    + book.getGenre() + "')";
             stmn.executeUpdate(sql);
             System.out.println("Libro creado correctamente!");
         } catch (Exception e) {
@@ -21,6 +22,6 @@ public class BookDAO {
             DBManager.closeConnection();
         }
 
-    } 
+    }
 
 }
