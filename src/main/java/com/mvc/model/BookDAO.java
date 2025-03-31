4276062b368ded9401eb37c5595e1d0b2afadb55
kat-lev <kat@katleverton.com>
2025-03-31 14:37:51 +0200

    add prepared statements in DAO

--- src/main/java/com/mvc/model/BookDAO.java
+++ src/main/java/com/mvc/model/BookDAO.java
@@ -1,21 +1,27 @@
 package com.mvc.model;
 
 import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 import com.mvc.config.DBManager;
 
 public class BookDAO {
 
+    private PreparedStatement stmn;
+    private Connection connection;
+
     public void createBook(Book book) {
         try {
-            Connection connection = DBManager.initConnection();
-            Statement stmn = connection.createStatement();
-            String sql = "INSERT INTO books (title, author, description, isbn, genre) VALUES ('" + book.getTitle()
-                    + "', '" + book.getAuthor() + "', '" + book.getDescription() + "', '" + book.getIsbn() + "', '"
-                    + book.getGenre() + "')";
-            stmn.executeUpdate(sql);
-            System.out.println("Libro creado correctamente!");
+            connection = DBManager.initConnection();
+            String sql = "INSERT INTO books (title, author, description, isbn, genre) VALUES (?, ?, ?, ?, ?)";
+            stmn = connection.prepareStatement(sql);
+            stmn.setString(1, book.getTitle());
+            stmn.setString(2, book.getAuthor());
+            stmn.setString(3, book.getDescription());
+            stmn.setString(4, book.getIsbn());
+            stmn.setString(5, book.getGenre());
+            stmn.executeUpdate();
+            System.out.println("El nuevo libro, "+book.getTitle()+ ", ha sido creado correctamente!");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally {
